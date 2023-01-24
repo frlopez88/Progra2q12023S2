f42b820482fb8ffd96fc3572e8a06152cd09ebae
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-01-23 20:44:42 -0600

    Commit Clase Persona

---
+++ Semana2/Semana2/Modelos/Persona.cs
@@ -0,0 +1,68 @@
+﻿
+namespace Semana2.Modelos
+{
+	public class Persona
+	{
+		private string nombre;
+		private DateTime fechaNacimiento;
+		private char genero;
+
+		public Persona(string nm, DateTime fn, char g )
+		{
+
+			this.nombre = nm;
+			this.fechaNacimiento = fn;
+			this.genero = g;
+
+		}
+
+		public string getNombre() {
+
+			return this.nombre;
+		}
+
+		public void setNombre(string nm) {
+
+			this.nombre = nm;
+
+		}
+
+		public DateTime getFechaNacimiento() {
+
+			return fechaNacimiento;
+		}
+
+		public void setFechaNacimiento(DateTime dt) {
+
+			this.fechaNacimiento = dt;
+		}
+
+		public char getGenero() {
+
+			return this.genero;
+		}
+
+		public void setGenero(char gn) {
+
+			this.genero = gn;
+		}
+
+		public int getEdad() {
+
+			int edad = 0;
+
+
+			edad = 2023 - this.fechaNacimiento.Year;
+
+			return edad;
+
+		}
+
+		public string toString() {
+
+			return $"Nombre: {this.nombre} - Fecha Nacimiento: {this.fechaNacimiento} - Genero:  {this.genero} ";
+		}
+
+	}
+}
+
--- Semana2/Semana2/Program.cs
+++ Semana2/Semana2/Program.cs
@@ -1,3 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Semana2.Modelos;
+
+DateTime fecha = new DateTime(1985, 02, 05);
+Persona p = new Persona("Cristiano Ronaldo", fecha, 'M');
+Persona p2 = new Persona("Leonel Messi", new DateTime(1988, 8, 30), 'M');
+Persona p3 = new Persona("Alex Morgan", new DateTime(1990, 1, 1), 'F');
+
+List<Persona> listaP = new List<Persona>();
+
+
+listaP.Add(p);
+listaP.Add(p2);
+listaP.Add(p3);
+
+
+foreach (Persona x in listaP) {
+
+    int edad=  x.getEdad();
+    Console.WriteLine(x.toString()  + " - Edad: "+ edad);
+
+}
+
 
